57f6a85e7010339057dd3aff1d9149c5d770a91e
H-kandil <kandilemail@gmail.com>
2025-07-01 20:15:06 +0200

    complete Mongo DB conection and express

--- server.js
+++ server.js
@@ -7,13 +7,17 @@ const app = express();
 
 // The Main settings إعدادات أساسية
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
 //Connecting to DB الاتصال بقاعدة البيانات
 mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB connected"))
-    .catch((err) => console.error("MongoDB error:", err));
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB Atlas"))
+    .catch((err) => console.error(" MongoDB Atlas error:", err));
+
 
 // Runing the Server تشغيل السيرفر
 const PORT = process.env.PORT || 5000;
