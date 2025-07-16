c92671811465449d9d9d27c28070f95e58498feb
H-kandil <kandilemail@gmail.com>
2025-07-16 18:50:16 +0200

    solving some bugs

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,16 +1,19 @@
 import jwt from "jsonwebtoken";
 
-const SECRET = process.env.JWT_SECRET; // اضبطه في .env
+const SECRET = process.env.JWT_SECRET;
 
-export const authMiddleware = (req, res, next) => {
-    const authHeader = req.headers.authorization; // يجب أن يكون "Bearer <token>"
-    if (!authHeader?.startsWith("Bearer "))
+export const protect = (req, res, next) => {
+    const authHeader = req.headers.authorization; // "Bearer <token>"
+
+    if (!authHeader?.startsWith("Bearer ")) {
         return res.status(401).json({ message: "No token" });
+    }
 
     const token = authHeader.split(" ")[1];
+
     try {
         const decoded = jwt.verify(token, SECRET);
-        req.user = decoded;
+        req.user = decoded; 
         next();
     } catch (err) {
         return res.status(403).json({ message: "Invalid token" });
